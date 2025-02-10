d463f47f9eeca98cc63af56dfb474663eb50eb68
HearttheaCAncla <heartthea.ancla@smc.pshs.edu.ph>
2025-02-10 11:02:29 +0800

    Update FA2.js
    final edit

--- FA2.js
+++ FA2.js
@@ -1,23 +1,33 @@
 function add(num1, num2) {
-    result(num1 + num2);
-  }
+  let a = num1 + num2;
+  result(a);
+  return a;
+}
 
 function sub(num1, num2) {
-    result(num1 - num2);
-  }
+  let a = num1 - num2;
+  result(a);
+  return a;
+}
 
 function mul(num1, num2) {
-    result(num1 * num2);
-  }
+  let a = num1 * num2;
+  result(a);
+  return a;
+}
 
 function div(num1, num2) {
-    result(num1 / num2);
-  }
+  let a = num1 / num2;
+  result(a);
+  return a;
+}
 
 function rem(num1, num2) {
-    result(num1 % num2);
-  }
+  let a = num1 % num2;
+  result(a);
+  return a;
+}
 
 function result(a) {
-    document.getElementById('ans').innerHTML = a;
-  } 
+  document.getElementById('ans').innerHTML = a;
+}
